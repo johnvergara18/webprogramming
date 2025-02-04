95ee103c9806d816beb84f9eec83777570ddbbdd
johnvergara18 <yaunjhonny@ici.edu.ph>
2025-02-04 16:52:17 +0800

    Add files via upload

---
+++ sample.php/p2_yaun.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Library Expansion Project</title>
+    <style>ZXX
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #BE3144;
+            text-align: center;
+            padding: 20px;
+        }
+        table {
+            margin: 0 auto;
+            border-collapse: collapse;
+            width: 70%;
+            background-color: #BE3144;
+        }
+        th, td {
+            border: 1px solid #ccc;
+            padding: 10px;
+            text-align: right;
+        }
+        th {
+            background-color: #BE3144;
+        }
+        caption {
+            font-size: 1.5em;
+            margin-bottom: 10px;
+        }
+        .footer {
+            text-align: left;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <caption>Public Library Expansion Project</caption>
+        <thead>
+            <tr>
+                <th>Expenditures</th>
+                <th>Estimated Cost</th>
+                <th>10% Increase</th>
+                <th>15% Increase</th>
+                <th>20% Increase</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Lumber</td>
+                <td>$150,000.00</td>
+                <td>$165,000.00</td>
+                <td>$172,500.00</td>
+                <td>$180,000.00</td>
+            </tr>
+            <tr>
+                <td>Concrete</td>
+                <td>$70,000.00</td>
+                <td>$77,000.00</td>
+                <td>$80,500.00</td>
+                <td>$84,000.00</td>
+            </tr>
+            <tr>
+                <td>Drywall</td>
+                <td>$60,000.00</td>
+                <td>$66,000.00</td>
+                <td>$69,000.00</td>
+                <td>$72,000.00</td>
+            </tr>
+            <tr>
+                <td>Paint</td>
+                <td>$50,000.00</td>
+                <td>$55,000.00</td>
+                <td>$57,500.00</td>
+                <td>$60,000.00</td>
+            </tr>
+            <tr>
+                <td>Miscellaneous</td>
+                <td>$20,000.00</td>
+                <td>$22,000.00</td>
+                <td>$23,000.00</td>
+                <td>$24,000.00</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Total Expenditures</th>
+                <td>$350,000.00</td>
+                <td>$385,000.00</td>
+                <td>$402,500.00</td>
+                <td>$420,000.00</td>
+            </tr>
+        </tfoot>
+    </table>
+    <div class="footer">
+        Created by: <em>jhonny yaun</em>
+    </div>
+</body>
+</html>  
